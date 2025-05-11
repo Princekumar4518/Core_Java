545531f866e4ae065e4822b0b45f7b353c69d3d7
Princekumar4518 <princekumajio03@gmail.com>
2025-05-11 10:43:20 +0530

    new file

---
+++ src/Basic01/Data_Types.java
@@ -0,0 +1,18 @@
+package Basic01;
+
+public class Data_Types {
+    public static void main(String[] args) {
+        byte num1=9;
+        System.out.println(num1);
+
+        int num2=40;
+        System.out.println(num2);
+
+        int sum=num1+num2;
+
+        System.out.println(sum);
+
+
+
+    }
+}
