f49b74783cdbf4608f8e79b9d991606e38e526d1
Princekumar4518 <princekumajio03@gmail.com>
2025-05-11 11:27:22 +0530

    new file

---
+++ src/Basic01/Task_01.java
@@ -0,0 +1,30 @@
+package Basic01;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        int num=5;
+        // using println
+        System.out.println(num+"x"+"1"+"="+"5");
+        System.out.println(num+"x"+"2"+"="+"10");
+        System.out.println(num+"x"+"3"+"="+"15");
+        System.out.println(num+"x"+"4"+"="+"20");
+        System.out.println(num+"x"+"5"+"="+"25");
+        System.out.println(num+"x"+"6"+"="+"30");
+        System.out.println(num+"x"+"7"+"="+"35");
+        System.out.println(num+"x"+"8"+"="+"40");
+        System.out.println(num+"x"+"9"+"="+"45");
+        System.out.println(num+"x"+"10"+"="+"50");
+
+
+        // using printf
+
+
+
+
+
+
+
+
+
+    }
+}
