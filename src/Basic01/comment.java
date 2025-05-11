3790f4236528ef01559bad5c486093c4c39da751
Princekumar4518 <princekumajio03@gmail.com>
2025-05-11 17:30:23 +0530

    add new

---
+++ src/Basic01/comment.java
@@ -0,0 +1,15 @@
+package Basic01;
+
+public class comment {
+    public static void main(String[] args) {
+        // This is a single line comment
+        // System.out.println("This is a comment");
+
+
+        /* System.out.println("This is a double line comment");
+          System.out.println("This is a double line comment");
+         * this is used when we add a long comment in the code like two
+         or three lines
+         */
+    }
+}
