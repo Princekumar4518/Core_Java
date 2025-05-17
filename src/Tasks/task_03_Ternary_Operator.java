018c51e20d20ec3e2af0b6a2ac6047c9641486ea
Princekumar4518 <princekumajio03@gmail.com>
2025-05-17 18:21:36 +0530

    Ternary operator Tasks

--- src/Tasks/task_03_Ternary_Operator.java
+++ src/Tasks/task_03_Ternary_Operator.java
@@ -2,8 +2,8 @@ package Tasks;
 
 public class task_03_Ternary_Operator {
     public static void main(String[] args) {
-        int Your_Age=5;
+        int Your_Age=5; // for checking you can changer age value
         String Verifying=Your_Age>=18?((Your_Age>=18&&Your_Age<=65)?"You are Adult":"You are Senior"):"you are Minor";
-        System.out.println(Verifying);
+        System.out.println(Verifying); //
     }
 }
